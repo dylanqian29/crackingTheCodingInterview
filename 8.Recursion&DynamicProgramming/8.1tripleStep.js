05b6ade66830ab11f7b602cfac100cfac6845728
Dylan Qian <dylan.qian.29@gmail.com>
2018-07-06 11:09:44 -0700

    added new 8.1 with different steps

--- 8.Recursion&DynamicProgramming/8.1tripleStep.js
+++ 8.Recursion&DynamicProgramming/8.1tripleStep.js
@@ -7,7 +7,7 @@ function tripleStep(k){
       counts++
       return;
     }
-
+    
     recursion(k-1)
     recursion(k-2)
     recursion(k-3)
@@ -18,3 +18,28 @@ function tripleStep(k){
 
 
 }
+
+
+//for different steps 
+// function tripleStep(k, steps) {
+//   let counts = 0
+
+
+//   let recursion = function (k) {
+//     if (k === 0) {
+//       counts++
+//       return;
+//     }
+//     if (k < 0) {
+//       return
+//     }
+//     steps.forEach(step => {
+//       recursion(k - step)
+//     })
+
+//   }
+//   recursion(k, steps)
+//   return counts
+// }
+
+// tripleStep(4, [1, 2])
